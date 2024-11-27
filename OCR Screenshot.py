65a430af1595b672cc1bbe37dee0c68cd78f5bb0
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2024-11-26 16:38:06 -0800

    OCR Screenshot.py

--- OCR Screenshot.py
+++ OCR Screenshot.py
@@ -3,153 +3,155 @@ import pytesseract
 import tkinter
 from PIL import ImageGrab
 
-# Variables para almacenar las coordenadas globales
-start_x, start_y, end_x, end_y = None, None, None, None
+# Variables para coordenadas
+x_ini, y_ini, x_fin, y_fin = None, None, None, None
 
-# Variable global para guardar el texto extraído
-extracted_text = ""
-
-# Configuración de pytesseract para que apunte al ejecutable de Tesseract en tu sistema
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"  # Cambia según tu ruta
+# Variable para texto extraído
+tmp_txt = ""
 
 # Función para capturar pantalla y extraer texto
-def capture_screen():
-    global extracted_text
+def capt_pant():
+    global tmp_txt
     
-    # Ajustar las coordenadas
-    x1 = min(start_x, end_x)
-    y1 = min(start_y, end_y)
-    x2 = max(start_x, end_x)
-    y2 = max(start_y, end_y)
+    # Definir límites de la captura
+    x1 = min(x_ini, x_fin)
+    y1 = min(y_ini, y_fin)
+    x2 = max(x_ini, x_fin)
+    y2 = max(y_ini, y_fin)
     
     # Capturar la pantalla en el área seleccionada
-    screenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+    tmp_capt = ImageGrab.grab(bbox=(x1, y1, x2, y2))
     
-    # Usar pytesseract para extraer texto de la imagen
-    extracted_text = pytesseract.image_to_string(screenshot)
-
-# Función para iniciar ventana secundaria
-def start_selection7():
-    # Iniciar la ventana secundaria en un hilo independiente
-    threading.Thread(target=run_selection_window).start()
+    # Extraer texto de la imagen
+    tmp_txt = pytesseract.image_to_string(tmp_capt)
 
 # Función para ejecutar la ventana secundaria
-def run_selection_window():
-    # Ocultar ventana principal usando after() para ejecutarlo en el hilo principal
-    main_window.root.after(0, main_window.withdraw)
+def f_vent_selec():
+    # Ocultar ventana principal
+    vent_princ.instancia_princ.after(0, vent_princ.f_ocu_vent_princ)
     
     # Instancia de la ventana secundaria
-    app = RectangularSelector()
+    vent_secu = vent_selecc()
     
     # Inicia el bucle de eventos para la ventana secundaria
-    app.root.mainloop()
+    vent_secu.instancia_secu.mainloop()
     
     # Cerrar bucle de la ventana secundaria al terminar la selección
-    app.root.destroy()
+    vent_secu.instancia_secu.destroy()
     
-    # Realiza la captura de pantalla después de cerrar la ventana de selección
-    capture_screen()
+    # Capturar pantalla después de selección
+    capt_pant()
     
-    # Mostrar ventana principal usando after() para ejecutarlo en el hilo principal
-    main_window.root.after(0, main_window.deiconify)
+    # Mostrar ventana principal
+    vent_princ.instancia_princ.after(0, vent_princ.f_most_vent_princ)
     
     # Actualizar la caja de texto con el texto extraído
-    main_window.root.after(0, main_window.update_text_box, extracted_text)
+    vent_princ.instancia_princ.after(0, vent_princ.f_act_caja_text, tmp_txt)
+
+# Función para iniciar ventana secundaria
+def f_vent_selec_hilo():
+    # Iniciar la ventana secundaria en un hilo independiente
+    threading.Thread(target=f_vent_selec).start()
 
 # Ventana secundaria (para la selección rectangular)
-class RectangularSelector:
+class vent_selecc:
+    # Constructor
     def __init__(self):
-        self.root = tkinter.Tk()
+        self.instancia_secu = tkinter.Tk()
         
-        # Poner la ventana en pantalla completa y sin bordes
-        self.root.attributes("-fullscreen", True)
-        self.root.configure(bg="black")
-        self.root.overrideredirect(True)
+        # Ventana en pantalla completa y sin bordes
+        self.instancia_secu.attributes("-fullscreen", True)
+        self.instancia_secu.configure(bg="black")
+        self.instancia_secu.overrideredirect(True)
         
-        # Configurar la transparencia de la ventana
-        self.root.attributes("-alpha", 0.4)
+        # Transparencia de la ventana
+        self.instancia_secu.attributes("-alpha", 0.4)
         
         # Canvas donde se dibujará el rectángulo
-        self.canvas = tkinter.Canvas(self.root, bg="black", bd=0, highlightthickness=0)
+        self.canvas = tkinter.Canvas(self.instancia_secu, bg="black", bd=0, highlightthickness=0)
         self.canvas.pack(fill="both", expand=True)
         
-        # Variables para las coordenadas del rectángulo
-        self.rect = None
+        # Variable para las coordenadas del rectángulo
+        self.rectxy = None
         
-        # Detecta los eventos de mouse en la ventana
-        self.canvas.bind("<ButtonPress-1>", self.on_mouse_down)
-        self.canvas.bind("<B1-Motion>", self.on_mouse_drag)
-        self.canvas.bind("<ButtonRelease-1>", self.on_mouse_up)
+        # Eventos de mouse en la ventana
+        self.canvas.bind("<ButtonPress-1>", self.mouse_izq_pres)
+        self.canvas.bind("<B1-Motion>", self.mouse_arr)
+        self.canvas.bind("<ButtonRelease-1>", self.mouse_izq_solt)
     
-    # Cuando el usuario hace clic, se inicia la selección del rectángulo
-    def on_mouse_down(self, event):
-        global start_x, start_y
+    # Función para selección de rectángulo con clic izquierdo
+    def mouse_izq_pres(self, event):
+        global x_ini, y_ini
         
-        start_x = event.x
-        start_y = event.y
+        # Captura de coordenadas
+        x_ini = event.x
+        y_ini = event.y
         
-        # Dibuja un rectángulo
-        self.rect = self.canvas.create_rectangle(start_x, start_y, start_x, start_y, outline="white", width=1, fill="grey")
+        # Dibujar un rectángulo
+        self.rectxy = self.canvas.create_rectangle(x_ini, y_ini, x_ini, y_ini, outline="white", width=1, fill="grey")
     
-    # Mientras el usuario arrastra, actualizar el rectángulo de selección
-    def on_mouse_drag(self, event):
+    # Función para actualizar el rectángulo de selección mientras se arrastra el mouse
+    def mouse_arr(self, event):
         cur_x, cur_y = event.x, event.y
-        self.canvas.coords(self.rect, start_x, start_y, cur_x, cur_y)
+        self.canvas.coords(self.rectxy, x_ini, y_ini, cur_x, cur_y)
     
-    # Cuando el usuario suelta el ratón, termina la selección
-    def on_mouse_up(self, event):
-        global end_x, end_y
+    # Función para al soltar clic izquierdo, terminar la selección
+    def mouse_izq_solt(self, event):
+        global x_fin, y_fin
         
-        # Guarda las coordenadas del borde inferior derecho del rectángulo
-        end_x, end_y = event.x, event.y
+        # Guardar las coordenadas del borde inferior derecho del rectángulo
+        x_fin, y_fin = event.x, event.y
         
-        # Cierra la ventana secundaria después de la selección
-        self.root.quit()
+        # Cerrar ventana secundaria después de la selección
+        self.instancia_secu.quit()
 
 # Ventana principal
-class MainWindow:
+class vent_princ:
+    # Constructor
     def __init__(self):
-        self.root = tkinter.Tk()
-        self.root.title("OCR Screenshot")
-        self.root.geometry("450x300")
-        self.root.configure(bg="white")
+        self.instancia_princ = tkinter.Tk()
+        
+        # Propiedades de la ventana principal
+        self.instancia_princ.title("OCR tmp_capt")
+        self.instancia_princ.geometry("450x300")
+        self.instancia_princ.configure(bg="white")
         
         # Botón para iniciar la selección
-        self.select_button = tkinter.Button(self.root, text="New", command=self.start_selectionx, height=2, width=20)
+        self.select_button = tkinter.Button(self.instancia_princ, text="New", command=self.f_click_vent_selec, height=2, width=20)
         # Agregar el botón con un margen
         self.select_button.pack(fill=tkinter.BOTH, expand=True, padx=10)
         
         # Caja de texto
-        self.text_box = tkinter.Text(self.root, font=("Arial", 11), bg="white", fg="black", bd=1)
+        self.text_box = tkinter.Text(self.instancia_princ, font=("Arial", 11), bg="white", fg="black", bd=1)
         self.text_box.pack(fill=tkinter.BOTH, expand=True, padx=10, pady=10)
     
-    # Iniciar ventana secundaria
-    def start_selectionx(self):
+    # Función para iniciar ventana secundaria
+    def f_click_vent_selec(self):
         # Ocultar la ventana principal antes de iniciar la selección
-        self.root.withdraw()
+        self.instancia_princ.withdraw()
         
         # Realizar selección de área en un hilo independiente
-        start_selection7()
+        f_vent_selec_hilo()
     
-    # Iniciar la ventana principal
-    def run(self):
-        self.root.mainloop()
+    # Función para iniciar la ventana principal
+    def f_ini_vent_princ(self):
+        self.instancia_princ.mainloop()
     
-    # Ocultar la ventana principal
-    def withdraw(self):
-        self.root.withdraw()
+    # Función para ocultar la ventana principal
+    def f_ocu_vent_princ(self):
+        self.instancia_princ.withdraw()
     
-    # Mostrar la ventana principal
-    def deiconify(self):
-        self.root.deiconify()
+    # Función para mostrar la ventana principal
+    def f_most_vent_princ(self):
+        self.instancia_princ.deiconify()
     
     # Función para actualizar caja de texto con texto extraído
-    def update_text_box(self, text):
+    def f_act_caja_text(self, text):
         # Insertar el texto nuevo al final del contenido actual
         self.text_box.insert(tkinter.END, text + "\n")
 
 # Crear la instancia de la ventana principal
-main_window = MainWindow()
+vent_princ = vent_princ()
 
 # Iniciar la aplicación
-main_window.run()
+vent_princ.f_ini_vent_princ()
