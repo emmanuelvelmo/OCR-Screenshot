0b7f505b5ed33ebc599dd76cb80556497b08f517
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-03-23 11:17:09 -0700

    OCR Screenshot.py

--- OCR Screenshot.py
+++ OCR Screenshot.py
@@ -3,6 +3,9 @@ import pytesseract
 import tkinter
 from PIL import ImageGrab
 
+# Ruta al ejecutable de Tesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 # Variables para coordenadas
 x_ini, y_ini, x_fin, y_fin = None, None, None, None
 
