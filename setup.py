f696a41e0610b37e97fa1935d35c856979b44f57
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-08-08 17:29:54 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from cx_Freeze import setup, Executable
 
-setup(name="OCR Screenshot", executables=[Executable("OCR Screenshot.py")], options={"build_exe": {"excludes": ["tkinter"]}})
+setup(name="OCR Screenshot", executables=[Executable("OCR Screenshot.py")])
